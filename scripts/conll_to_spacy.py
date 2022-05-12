adf0153a52acea1377bfa895adb781c8d53e5e8a
Ephraim Berkovitch <ephraim.berkovitch@gmail.com>
2022-05-12 11:22:57 -0400

    1st version of a model

--- scripts/conll_to_spacy.py
+++ scripts/conll_to_spacy.py
@@ -13,3 +13,12 @@ for line in lines:
     tokens_num = len(line.split('\t'))
     if tokens_num != 10:
         print(line.strip('\n'), tokens_num, index)
+    try:
+        id = int(line.split('\t')[0])
+    except:
+        try:
+            parts = line.split('\t')[0].split('-')
+            id1 = int(parts[0])
+            id2 = int(parts[1])
+        except:
+            print(line.strip('\n'), tokens_num, index)
